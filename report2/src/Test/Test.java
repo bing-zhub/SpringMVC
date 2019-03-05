c06ec86be3d6cbe785f01846ab89f5573227f97c
bing.zhub <bing.zhub@gmail.com>
2019-03-05 09:16:58 +0800

    update report 2

--- report2/src/Test/Test.java
+++ report2/src/Test/Test.java
@@ -1,4 +1,25 @@
 package Test;
 
-public class Test {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name= "Test", urlPatterns = "/test")
+public class Test extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.setContentType("text/plain");
+        PrintWriter out = resp.getWriter();
+        out.println("Test Passed");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    
+    }
 }
